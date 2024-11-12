a1f21b8d9d8f2bbbe8e301726f52163d0710b23f
archijs24 <archanasalunke137@gmail.com>
2024-11-12 20:14:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const cors=require("cors");
+const mysql=require("mysql2");
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+const con=mysql.createConnection({
+	host:"sql12.freesqldatabase.com",
+	user:"sql12744339",
+	password:"T9gdwiZVhQ",
+	database:"sql12744339"
+});
+
+app.post("/save",(req,res)=>{
+		let data=[req.body.name,req.body.feedback];
+		let sql="insert into student values(?,?)";
+		con.query(sql,data,(err,result)=>{
+			if(err)  	res.send(err);
+			else		res.send(result);
+	});
+});
+app.listen(8000,()=>{console.log("ready@8000");});
